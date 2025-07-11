fdf5a8e9bcc65d4bd9d166a7021eaa17c245b83d
lewiskarume <116207640+lubbacris@users.noreply.github.com>
2025-07-11 06:07:53 +0200

    completed week01 exercise

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -1,9 +1,18 @@
 using System;
+using System.ComponentModel;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        // Adding line divider for better visibility
+        Console.WriteLine("====================================================>");
+        Console.Write("What is your first name? ");
+        string first_name = Console.ReadLine();
+        Console.WriteLine(first_name);
+        Console.Write("What is your last name? ");
+        string last_name = Console.ReadLine();
+        Console.WriteLine(last_name);
+        Console.WriteLine($"Your name is {last_name}, {first_name} {last_name}.");
     }
 }
