e596102bec25c57a74e0d8b8af138d59f5e446ef
lewiskarume <116207640+lubbacris@users.noreply.github.com>
2025-07-15 22:54:15 +0200

    Worked with Functions

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -4,6 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise5 Project.");
+        DisplayWelcome();
+        
+        string user_name = PromptUserName();
+        
+        int user_number = PromptUserNumber();
+        
+        int squared_number = SquareNumber(user_number);
+        
+        DisplayResult(user_name, squared_number);
+    }
+
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome!");
+    }
+    static string PromptUserName()
+    {
+        Console.Write("Please enter your name: ");
+        string name = Console.ReadLine();
+        return name;
+    }
+
+    static int PromptUserNumber()
+    {
+        Console.Write("Please enter your favorite number: ");
+        int number = int.Parse(Console.ReadLine());
+        return number;
+    }
+
+    static int SquareNumber(int number)
+    {
+        return number * number;
+    }
+
+    static void DisplayResult(string user_name, int squared_number)
+    {
+        Console.WriteLine($"{user_name}, the square of your number is {squared_number}");
     }
 }
