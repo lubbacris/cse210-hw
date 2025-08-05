7ff036bc38981a272c65e14fd132ba0bcd8b5119
lewiskarume <116207640+lubbacris@users.noreply.github.com>
2025-08-05 21:14:10 +0200

    Submitting homework assigment

---
+++ week05/Homework/Assignment.cs
@@ -0,0 +1,21 @@
+public class Assignment
+{
+    private string _studentName;
+    private string _topic;
+
+    public Assignment(string studentName, string topic)
+    {
+        _studentName = studentName;
+        _topic = topic;
+    }
+    public string GetSummary()
+    {
+        return $"{_studentName} - {_topic}";
+    }
+
+    // Protected getter for derived classes
+    protected string GetStudentName()
+    {
+        return _studentName;
+    }
+}
